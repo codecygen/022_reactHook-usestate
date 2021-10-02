5c7704aa1535264ac85b6f50790cdea65b861bbd
gingercook <vahitaras@gmail.com>
2021-10-01 20:29:41 -0400

    App.jsx file content done.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  return <div />;
+const App = () => {
+
+  let count = 0;
+
+  let increase = () => {
+    count++;
+    ReactDOM.render(
+      <div className="container">
+        <h1>{count}</h1>
+        <button onClick={increase}>+</button>
+      </div>,
+      document.getElementById("root")
+    );
+    
+  }
+  
+    return(
+      <div className="container">
+        <h1>{count}</h1>
+        <button onClick={increase}>+</button>
+      </div>
+    )
 }
+
 
 export default App;
