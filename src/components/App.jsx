4eeb60f0304e67e34cf9def108ecb82e0eca6668
gingercook <vahitaras@gmail.com>
2021-10-02 20:32:34 -0400

    get time button added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-// import React, { useState } from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
 
-  const [count, setCount] = React.useState(0);
-  // const [count, setCount] = useState(0);
+  // const [count, setCount] = React.useState(0);
+  const [count, setCount] = useState(0);
 
   const increase = () => {
     setCount(count + 1);
@@ -15,13 +14,24 @@ const App = () => {
     setCount(count - 1);
   }
 
+  const [initialTime, setTime] = useState('Time');
+  const getTime = () => {
+    setTime(new Date().toLocaleTimeString());
+  }
+
     return(
-      <div className="container">
-        <h1>{count}</h1>
-        <button onClick={decrease}>-</button>
-        <button onClick={increase}>+</button>
+      <div>
+        <div className="container">
+          <h1>{count}</h1>
+          <button onClick={decrease}>-</button>
+          <button onClick={increase}>+</button>
+        </div>
+        <div className="container">
+          <h1 class='watch'>{initialTime}</h1>
+          <button onClick={getTime} class="time-btn">Get Time</button>
+        </div>
       </div>
-    )
+    );
 }
 
 
